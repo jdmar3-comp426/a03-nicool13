46d87b88987f21f26e3fd322938f9fbb456218bd
Nicole Sherman <nicool@live.unc.edu>
2021-11-11 14:53:59 -0500

    declared vars

--- src/mild/mild_1.js
+++ src/mild/mild_1.js
@@ -9,7 +9,7 @@
  * see https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Template_literals
  */
 export function sumToString(a, b) {
-    str = "'" + a + " + " + b + " = (" + a+b + ")'"
+    let str = "'" + a + " + " + b + " = (" + a+b + ")'"
     print(str)
     return str
 }
@@ -26,7 +26,7 @@ export function sumToString(a, b) {
  *
  */
 export function getIncreasingArray(startNumber, endNumber) {
-    arr = new array[endNumber-startNumber+1]
+    let arr = new array[endNumber-startNumber+1]
     for (i = startNumber; i <= endNumber; i++) {
         arr[i-startNumber] = i    
     }
@@ -41,8 +41,8 @@ export function getIncreasingArray(startNumber, endNumber) {
  * and https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math
  */
 export function maxAndMin(numbers) {
-    max = numbers[0]
-    min = numbers[0]
+    let max = numbers[0]
+    let min = numbers[0]
     for (i = 1; i< numbers.length; i++) {
         if (numbers[i] > max) {
             max = numbers[i]

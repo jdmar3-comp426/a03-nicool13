c40f20868ab7d4e18b4d5d8bede03f3e4ba9e159
Nicole Sherman <nicool@live.unc.edu>
2021-11-11 15:02:01 -0500

    trying

--- src/mild/mild_1.js
+++ src/mild/mild_1.js
@@ -11,7 +11,8 @@
  * see https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Template_literals
  */
 export function sumToString(a, b) {
-    let str = "'" + a + " + " + b + " = (" + a+b + ")'"
+    var sum = a+b
+    var str = "'" + a + " + " + b + " = (" + sum + ")'"
     return str
 }
 
@@ -27,8 +28,8 @@ export function sumToString(a, b) {
  *
  */
 export function getIncreasingArray(startNumber, endNumber) {
-    let arr = new array[endNumber-startNumber+1]
-    for (i = startNumber; i <= endNumber; i++) {
+    var arr = new Array(endNumber-startNumber+1)
+    for (var i = startNumber; i <= endNumber; i++) {
         arr[i-startNumber] = i    
     }
     return arr
@@ -42,22 +43,18 @@ export function getIncreasingArray(startNumber, endNumber) {
  * and https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math
  */
 export function maxAndMin(numbers) {
-    let max = numbers[0]
-    let min = numbers[0]
-    for (i = 1; i< numbers.length; i++) {
-        if (numbers[i] > max) {
-            max = numbers[i]
+    var maxi = numbers[0]
+    var mini = numbers[0]
+    for (var i = 1; i< numbers.length; i++) {
+        if (numbers[i] > maxi) {
+            maxi = numbers[i]
         }
-        if (numbers[i] < min) {
-            min = numbers[i]
+        if (numbers[i] < mini) {
+            mini = numbers[i]
         } 
     }
-    return MM(min, max)
-}
-
-function MM(min, max) {
-    this.min = min
-    this.max = max
+    var ans = {min: mini, max: maxi}
+    return ans
 }
 
 /**

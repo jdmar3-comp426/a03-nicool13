d05cb43f62fe61238448877968a95bd570106edf
Nicole Sherman <nicool@live.unc.edu>
2021-11-11 16:10:04 -0500

    fugghhg

--- src/mild/mild_1.js
+++ src/mild/mild_1.js
@@ -86,7 +86,7 @@ export function countArray(array) {
     
     var ans = {}
     for (var i = 0; i < counts.length; i++) {
-        key = String(unique[i])
+        var key = String(unique[i])
         Object.defineProperty(ans, key, {enumerable: true, writable: true})
         ans.key = counts[i]
     }

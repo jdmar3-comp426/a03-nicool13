80dce7c29514c8040311d5c6fdc58710e6917ed2
Nicole Sherman <nicool@live.unc.edu>
2021-11-17 10:14:20 -0500

    maybe all done

--- src/spicy/spicy_9.js
+++ src/spicy/spicy_9.js
@@ -160,9 +160,27 @@ export const someEven = (arr, test) => {
  *       -->  { pass: [1, 5, 31], fail: [90] }
  */
 export const filter = (arr, test) => {
+    var pass = []
+    var fai = []
 
+    for (var i = 0; i < arr.length; i++) {
+        if (test(arr[i])) {
+            pass.push(arr[i])
+        } else {
+            fai.push(arr[i])
+        }
+    }
+
+    return {"pass": pass, "fail": fai}
 };
 
+function testOdds(num) {
+        if (num % 2 == 0) {
+            return false
+        } else {
+            return true
+        }
+}
 
 /**
  * Write and export a function named "allEvensAreOdd" which takes as input an
@@ -170,7 +188,7 @@ export const filter = (arr, test) => {
  *   odd numbers. Use the "everyEven" function in this function.
  */
 export const allEvensAreOdd = (arr) => {
-
+    return everyEven(arr, testOdds)
 };
 
 
@@ -180,7 +198,7 @@ export const allEvensAreOdd = (arr) => {
  *   array is an odd number. Use the "someEven" function in this function.
  */
 export const anEvenIsOdd = (arr) => {
-
+    return someEven(arr, testOdds)
 };
 
 
@@ -191,5 +209,10 @@ export const anEvenIsOdd = (arr) => {
  *   pass the test. You must use the filter function.
  */
 export const hasExactly = (arr, test, n) => {
-
+    var returnedObj = filter(arr, test)
+    if (returnedObj.pass.length == n) {
+        return true
+    } else {
+        return false
+    }
 };
